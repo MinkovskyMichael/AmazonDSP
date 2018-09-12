dbeb53e0a473ea52cea5d76deef55fba2860be46
Wilkenson Francois <wilkensonfrancois@gmail.com>
2018-09-12 13:22:21 -0400

    added headers

--- index.js
+++ index.js
@@ -14,14 +14,25 @@
     var parsedUrl = url.parse(req.url, true);
 
     // Get the path
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    // Get the query string as an object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP Method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
 
     //Send the response
     res.end('Hello World\n');
 
     // Log the request path
-    
- });
+        console.log('Request received with these headers: ', headers);
+
+});
 
     // Start the server, and have it listen on port 3000
     server.listen(3000, function(){
