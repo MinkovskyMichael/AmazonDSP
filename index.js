3ad5e0759b2e79d4ee333c5c60d7b5d16cf8b009
Wilkenson Francois <wilkensonfrancois@gmail.com>
2018-09-12 09:09:46 -0700

    modified index to reflect parse url, get path, send response and 	log path

--- index.js
+++ index.js
@@ -3,15 +3,27 @@
  * 
  */
  
- // Dependencies
- var http = require('http');
+    // Dependencies
+    var http = require('http');
+    var url = require('url');
 
- // The server should respond to all requests with a string
- var server = http.createServer(function(req, res){
+    // The server should respond to all requests with a string
+    var server = http.createServer(function(req, res){
+    
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+
+
+    //Send the response
     res.end('Hello World\n');
+
+    // Log the request path
+    
  });
 
- // Start the server, and have it listen on port 3000
- server.listen(3000, function(){
-     console.log("THe server is listening on port 3000 now");
+    // Start the server, and have it listen on port 3000
+    server.listen(3000, function(){
+        console.log("The server is listening on port 3000 now");
  });
